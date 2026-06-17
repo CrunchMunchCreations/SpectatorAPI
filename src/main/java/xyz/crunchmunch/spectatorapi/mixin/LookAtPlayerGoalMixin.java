cb9ffc2d55232406f3d0db681bd8bd9fb356451b
BluSpring <bluspringindohq@gmail.com>
2026-06-18 06:46:53 +0800

    Update to 26.2

--- src/main/java/xyz/crunchmunch/spectatorapi/mixin/LookAtPlayerGoalMixin.java
+++ src/main/java/xyz/crunchmunch/spectatorapi/mixin/LookAtPlayerGoalMixin.java
@@ -2,16 +2,17 @@ package xyz.crunchmunch.spectatorapi.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import xyz.crunchmunch.spectatorapi.SpectatorAPI;
 
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+
 @Mixin(LookAtPlayerGoal.class)
 public abstract class LookAtPlayerGoalMixin {
-    @ModifyReturnValue(method = "method_18414", at = @At("RETURN"))
+    @ModifyReturnValue(method = "lambda$canUse$0", at = @At("RETURN"))
     private static boolean checkIfPlayerIsSpectator(boolean original, @Local(argsOnly = true) LivingEntity living) {
         if (living instanceof ServerPlayer player && SpectatorAPI.isCustomSpectator(player))
             return false;
